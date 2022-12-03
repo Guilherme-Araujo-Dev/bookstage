64d264e155f4d89ed4b9faed49bb187b7de4e451
Guilherme Araujo <guilhermearaujop26@gmail.com>
2022-12-03 18:55:12 -0300

    Começando a desenvolver o refresh automatico

--- Public/Chat/Paginas/usuarios.php
+++ Public/Chat/Paginas/usuarios.php
@@ -1,71 +1,56 @@
 <?php
-include_once("../../Class/Conexao.php");
+include_once("../../Class/conexao.php");
 $conn = conectar();
 ?>
-<!DOCTYPE html>
-<html>
 
-<head>
-    <meta name="viewpot" content="width=device-width, inicial-scale=1">
-    <title>Bookstage</title>
-    <link rel="stylesheet" href="../../../css/1.css">
-    <link rel="icon" href="../../../imag/logo.jpg">
-    <link rel="stylesheet" href="../css/style.css">
-</head>
+<div id="content">
+    <?php
+    $sql = "SELECT * FROM leitores";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
 
-<body>
-    <main>
-        <div id="content">
-            <?php
-            $sql = "SELECT * FROM leitores";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
+    if ($stmt->rowCount() <= 0) {
+        echo "Nenhum usuário encontrado";
+    } else {
+        // $nome    = $row['nome'];
+        // $usuario = $row['usuario'];
+        // $foto    = $row['foto'];
 
-            if ($stmt->rowCount() <= 0) {
-                echo "Nenhum usuário encontrado";
-            } else {
-                // $nome    = $row['nome'];
-                // $usuario = $row['usuario'];
-                // $foto    = $row['foto'];
-                
-                $sql = 'SELECT * FROM mensagens WHERE (id_de = ? AND id_para = ?) OR (id_para = ? AND id_de = ?)';
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1,  $_SESSION['idusuario']);
-                $stmt->bindParam(2,  $_SESSION['usuCon'] );
-                $stmt->bindParam(3,  $_SESSION['idusuario']);
-                $stmt->bindParam(4,  $_SESSION['usuCon'] );
-                
-                $stmt->execute();
-                $men = $stmt->fetchAll();
+        $sql = 'SELECT * FROM mensagens WHERE (id_de = ? AND id_para = ?) OR (id_para = ? AND id_de = ?)';
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(1,  $_SESSION['idusuario']);
+        $stmt->bindParam(2,  $_SESSION['usuCon']);
+        $stmt->bindParam(3,  $_SESSION['idusuario']);
+        $stmt->bindParam(4,  $_SESSION['usuCon']);
 
-                foreach ($men as $m) {
-            ?>
-                    <table>
-                        <tr>
-                            <!-- <td><img src="<?php echo $foto; ?>" class="foto-user"></td> -->
-                            <?php 
-                            if($m['id_de'] == $_SESSION['idusuario']) { ?>
-                                <td> <b>Você:</b> <?php echo $m['mensagem']; ?></td>
-                                <?php } else {?>
-                                <td><b><?php echo $_SESSION['nomeUsuCon'] . ":</b> " . $m['mensagem']; ?></td>
-                                <?php }
-                            ?>
-                        </tr>
-                    </table>
-                    <hr />
-            <?php }
-            } ?>
-        </div>
-    </main>
-    <script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
-    <script>
-        $(".bmenu").click(function() {
-            $(".menu").show();
-        });
-        $(".btn").click(function() {
-            $(".menu").hide();
-        });
-    </script>
-</body>
+        $stmt->execute();
+        $men = $stmt->fetchAll();
 
-</html>
+        foreach ($men as $m) {
+    ?>
+            <table>
+                <tr>
+                    <!-- <td><img src="<?php echo $foto; ?>" class="foto-user"></td> -->
+                    <?php
+                    if ($m['id_de'] == $_SESSION['idusuario']) { ?>
+                        <td> <b>Você:</b> <?php echo $m['mensagem']; ?></td>
+                    <?php } else { ?>
+                        <td><b><?php echo $_SESSION['nomeUsuCon'] . ":</b> " . $m['mensagem']; ?></td>
+                    <?php }
+                    ?>
+                </tr>
+            </table>
+            <hr />
+    <?php }
+    } ?>
+</div>
+
+<script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script>
+    $(".bmenu").click(function() {
+        $(".menu").show();
+    });
+    $(".btn").click(function() {
+        $(".menu").hide();
+    });
+</script>
