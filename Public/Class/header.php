0201998b83aa65c5ca96f9408db8b271f0c458a1
Guilherme Araujo <guilhermearaujop26@gmail.com>
2022-12-03 11:57:57 -0300

    Arrumando mais um poquinho do style

---
+++ Public/Class/header.php
@@ -0,0 +1,39 @@
+<header class="cabecalho">
+    <a href="../index.php">
+        <h1 class="logo"></h1>
+    </a>
+    <button class="bmenu">
+        <img src="../../imag/menu.png">
+    </button>
+    <nav class="menu">
+        <a class="btn"><i>X</i></a>
+        <ul>
+            <li><a href="../index.php">In&iacute;cio</a></li>
+            <li class="li"><a href="#">Gen&ecirc;ro:
+                    <select id="link" onchange="location = this.value;">
+                        <option></option>
+                        <option class="option1" value="../Generos/acao.php">A&ccedil;&atilde;o</option>
+                        <option class="option2" value="../Generos/aventura.php">Aventura</option>
+                        <option class="option3" value="../Generos/comedia.php">Com&eacute;dia</option>
+                        <option class="option4" value="../Generos/drama.php">Drama/Trag&eacute;dia</option>
+                        <option class="option5" value="../Generos/fabula.php">F&aacute;bula</option>
+                        <option class="option6" value="../Generos/fantasia.php">Fantasia</option>
+                        <option class="option7" value="../Generos/ficcao.php">Fic&ccedil;&atilde;o</option>
+                        <option class="option8" value="../Generos/lgbtqia+.php">LGBTQIA+</option>
+                        <option class="option9" value="../Generos/misterio.php">Mist&eacute;rio</option>
+                        <option class="option10" value="../Generos/romance.php">Rom&acirc;nce</option>
+                        <option class="option11" value="../Generos/sobrenatural.php">Sobrenatural</option>
+                        <option class="option12" value="../Generos/suspense.php">Suspense</option>
+                        <option class="option13" value="../Generos/terror.php">Terror</option>
+                        <option class="option14" value="../Generos/horror.php">Horror</option>
+                    </select>
+                </a></li>
+            <li><a href="favoritos.php">Favoritos</a></li>
+            <li><a href="ja li.php">Ja li</a></li>
+            <li><a href="queroler.php">Quero ler</a></li>
+            <li><a href="perfil.php">Perfil</a></li>
+            <li><a href="chat/paginas/chat.php">Chat</a></li>
+            <li><a href="chat/paginas/pesquisa.php">Pesquisar Usuários</a></li>
+        </ul>
+    </nav>
+</header>
