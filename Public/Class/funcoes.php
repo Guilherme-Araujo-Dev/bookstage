adbc855a06244c1246af5f06f459ae0955502e55
Guilherme Araujo <guilhermearaujop26@gmail.com>
2022-12-04 16:29:30 -0300

    Agora sim ta certo

--- Public/Class/funcoes.php
+++ Public/Class/funcoes.php
@@ -1,22 +1,4 @@
 
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Bookstage</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="../../CSS/1.css">
-    <link rel="stylesheet" href="../../CSS/2.css">
-    <link rel="icon" href="../../IMG/imag/logo.jpg">
-</head>
-
-<body>
-    <?php include("../Class/header.php") ?>
-    <main>
-        <div class="conteudo">
-            Favoritos
-        </div>
-
         <div class="livros" id="imagens">
             <?php foreach($livros as $l) { 
                 $sql = "SELECT * FROM livros WHERE idlivros = ?";
--- Public/Funcoes/queroler.php
+++ Public/Funcoes/queroler.php
@@ -10,6 +10,23 @@ $stmt->execute();
 
 $livros = $stmt->fetchAll();
 
-include("../Class/funcoes.php");
-
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Bookstage</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../../CSS/1.css">
+    <link rel="stylesheet" href="../../CSS/2.css">
+    <link rel="icon" href="../../IMG/imag/logo.jpg">
+</head>
+
+<body>
+    <?php include("../Class/header.php") ?>
+    <main>
+        <div class="conteudo">
+            Favoritos
+        </div>
+        <?php include("../Class/funcoes.php"); ?>
